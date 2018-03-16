deb76f024085de175f860218a948ae4674a3a45c
Stephen Gulland <Stephen Gulland@Obsidian-Brick>
2018-03-15 21:54:16 -0700

    Initial AVL Node file with comments about implementation.

---
+++ src/refined/AVLNode.java
@@ -0,0 +1,37 @@
+package refined;
+
+public class AVLNode {
+	//PRIVATE MEMBERS
+	//Height of the Node
+	int height;
+	//Object contained by the Node
+	//Left Child Node
+	AVLNode leftChild;
+	//Right Child Node
+	AVLNode  rightChild;
+	
+	//PUBLIC METHODS
+	//Standard Constructor with Object
+	//Constructor with Object and Parent
+	//Set node Height
+	//Return the Node Height
+	//Set Node Object
+	//Return a Copy of the Node's Object
+	//Return the Balance Value of this Node; Where Balance is the Height of the Right Node minus Height of the Left Node
+	
+	//Set the Left Child with a given Node; Set that Node's Parent as this node
+	//Return a Reference to the Left Child
+	//Return a copy of Left Child's Object
+	//Remove this node from the left child as a parent; Pop the Left Child; 
+	
+	//Set the Right Child with a given Node; Set that Node's Parent as this Node
+	//Return a reference to the Right Child
+	//Return a copy of Left Child's Object
+	//Remove  this node from the right child as a parent; Pop the Right Child;
+	
+	//Set this Node's Parent
+	//Get a Reference to this Node's Parent
+	//Get the Value Contained By this Node's Parent? I don't think I need this functionality
+	
+	//Print all the information about this node; Includes: Value, Height, Parent, Balance, and Children
+}
